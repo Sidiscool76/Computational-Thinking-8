c797161b1ae59376cfcecd90bdece44e1f40bade
Sidiscool76 <sidmignone@seattleacademy.org>
2025-01-15 22:41:03 +0000

    codeart.py

---
+++ Projects/code_art_intro.py
@@ -0,0 +1,33 @@
+# ###############################################
+# ### SETUP ###
+import turtle
+# ###############################################
+
+t = turtle.Turtle()
+t.penup()
+t.goto(-100, -150)
+t.color("red")
+t.pendown()
+
+
+# repeat 6 times
+for i in range(5000):
+    t.forward(16)
+    t.left(13)
+colors = ["gray","black","red"]
+for i in range ( n ):
+    t.forward ( distance + i)
+    t.left ( angle)
+
+
+t.left(10)
+t.forward(167)
+t.left(41)
+t.forward(73)
+
+
+
+# ###############################################
+# ### ENDING ###
+turtle.exitonclick()
+# ###############################################
---
+++ code_art.py
@@ -0,0 +1,17 @@
+
+import turtle
+import random
+
+t = turtle.Turtle()
+t.speed(10)
+
+# Random triangles
+colors = ["red", "black", "blue", "green", "yellow", "purple"]  
+angles = [0, 60, 120, 180, 240, 300] 
+
+for i in range(999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999):
+    t.color(colors[i % 6]) 
+    t.forward(10) 
+    t.left(random.choice(angles)) 
+
+turtle.exitonclick() 
