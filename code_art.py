529f3ab71275b4500968d96ee81e916fae6a8d36
Sidiscool76 <sidmignone@seattleacademy.org>
2025-01-29 19:54:57 +0000

    code_art.py

--- code_art.py
+++ code_art.py
@@ -1,14 +1,14 @@
 
 import turtle
 import random
-
+# Speed
 t = turtle.Turtle()
 t.speed(10)
 
 # Random triangles
 colors = ["red", "black", "blue", "green", "yellow", "purple"]  
 angles = [0, 60, 120, 180, 240, 300] 
-
+# Movement
 for i in range(999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999):
     t.color(colors[i % 6]) 
     t.forward(10) 
